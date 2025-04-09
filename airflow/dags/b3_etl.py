ab609d9d92f18143afb696779ffd9ccc84448295
Airton Carneiro <airtoncarneiro@gmail.com>
2025-04-09 19:55:22 +0000

    Refactor logging and task structure in b3_etl DAG: streamline logging functions and enhance task execution flow

--- airflow/dags/b3_etl.py
+++ airflow/dags/b3_etl.py
@@ -2,81 +2,78 @@ from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 from datetime import timedelta
 import logging
-import src.config.logging_config as log
 from typing import Dict, Any
 from pathlib import Path
 import os
 
-from src.services.downloader import Downloader
-from src.services.file_handler import FileHandler
-from src.services.zip_extractor import ZipExtractor
 
 
-
-# Configuração de Header e Trailing no logging
-def log_banner(mensagem: str, tipo: str = "info"):
-    borda = "=" * 80
-    msg = f"\n{borda}\n📢 {mensagem}\n{borda}\n"
-    getattr(logging, tipo)(msg)
-
-
-# 🚀 Wrappers com @task para o Airflow
-@task
-def download_zip_file() -> str:
-    # Define save_dir
-    save_dir = os.path.join(os.path.dirname(__file__), "downloads")
-    os.makedirs(save_dir, exist_ok=True)
-    # Realiza o download do arquivo zip e salva no diretório
-    downloader = Downloader()
-    zip_data = downloader._download_zip_file()
-    saved_path = FileHandler._save_file_to_disk(zip_data, save_dir)
-    return saved_path
-
-@task
-def extract_zip_file(zip_path: str) -> Dict[str, Any]:
-    zip_name = Path(zip_path).stem
-    zip_dir = Path(zip_path).parent
-    extract_dir = f"{zip_dir}/extracted_txt/{zip_name}"
-    
-    extractor = ZipExtractor(extract_dir)  # Agora é um diretório válido
-    result = extractor._extract_zip_file(zip_path)
-    return result
-
-# 📅 DAG do Airflow
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "retries": 0,
     "retry_delay": timedelta(minutes=5),
 }
-dag_name = "b3_etl"
+
+@task
+def log_start():
+    logger = logging.getLogger("airflow.task")
+    logger.info(f"INICIANDO DAG: b3_etl")
+    return None
+
+@task
+def log_end():
+    logger = logging.getLogger("airflow.task")
+    logger.info(f"FINALIZOU DAG: b3_etl")
+    return extracted
+
+@task
+def download_zip_file() -> str:
+    from src.services.downloader import Downloader
+    from src.services.file_handler import FileHandler
+    logger = logging.getLogger("airflow.task")
+    save_dir = os.path.join(os.path.dirname(__file__), "downloads")
+    os.makedirs(save_dir, exist_ok=True)
+    downloader = Downloader()
+    zip_data = downloader._download_zip_file()
+    saved_path = FileHandler._save_file_to_disk(zip_data, save_dir)
+    logger.info(f"Arquivo ZIP baixado e salvo em: {saved_path}")
+    return saved_path
+
+@task
+def extract_zip_file(zip_path: str) -> Dict[str, Any]:
+    from src.services.zip_extractor import ZipExtractor
+    logger = logging.getLogger("airflow.task")
+    zip_name = Path(zip_path).stem
+    zip_dir = Path(zip_path).parent
+    extract_dir = f"{zip_dir}/extracted_txt/{zip_name}"
+    extractor = ZipExtractor(extract_dir)
+    result = extractor._extract_zip_file(zip_path)
+    logger.info(f"Arquivo ZIP extraído em: {extract_dir}")
+    logger.info(f"Arquivos extraídos: {result['extracted_files']}")
+    return result
+
 @dag(
-    dag_id=dag_name,
+    dag_id="b3_etl",
     default_args=default_args,
     schedule=None,
     start_date=days_ago(1),
     catchup=False,
-    tags=["b3", "etl", "v6"],
+    tags=["b3", "etl", "v 11"],
     description="Pipeline híbrido testável como DAG ou script Python (debugável)"
 )
 def final_download_and_extract_zip():
-    log_banner(f"INICIANDO DAG: {dag_name}")
-    if DEBUG_MODE:
-        # Modo debug: executa as funções diretamente para possibilitar o teste
-        downloaded = download_zip_file.function()
-        extracted = extract_zip_file.function(downloaded) # type: ignore
-    else:
-        # Modo Airflow: cria as tasks e suas dependências, permitindo a visualização do gráfico
-        downloaded = download_zip_file()
-        extracted = extract_zip_file(downloaded) # type: ignore
-    
-    log_banner(f"FINALIZOU DAG: {dag_name}")
-    return extracted
+    start = log_start()
+    downloaded = download_zip_file()
+    extracted = extract_zip_file(downloaded)    # type: ignore
+    end = log_end()    # type: ignore
 
-# Configuração do logging
-log.logging.basicConfig()
-
-# Flag para debug com base na variável de ambiente
-DEBUG_MODE = os.environ.get("DEBUG_MODE", "false").lower() == "true"
+    start >> downloaded >> extracted >> end # type: ignore
+    return end
 
 dag = final_download_and_extract_zip()
+
+if __name__ == "__main__" and os.environ.get("DEBUG_MODE", "false").lower() == "true":
+    print("Executando em modo debug...")
+    downloaded = download_zip_file.function()
+    extracted = extract_zip_file.function(downloaded)
