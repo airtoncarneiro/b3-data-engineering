d3f27d47396c109a45de44190dc7c521899a5ec2
Airton Carneiro <airtoncarneiro@gmail.com>
2025-04-10 17:20:59 +0000

    Add initialization for B3_DOWNLOAD_ALL variable in init-airflow.sh

--- .devcontainer/init-airflow.sh
+++ .devcontainer/init-airflow.sh
@@ -22,4 +22,12 @@ else
     echo "👤 Usuário admin já existe!"
 fi
 
+# Cria a variável B3_DOWNLOAD_ALL se ela não existir
+if ! airflow variables get B3_DOWNLOAD_ALL >/dev/null 2>&1; then
+    airflow variables set B3_DOWNLOAD_ALL "true"
+    echo "✅ Variável B3_DOWNLOAD_ALL criada com valor 'true'!"
+else
+    echo "👤 Variável B3_DOWNLOAD_ALL já existe!"
+fi
+
 echo -e "✅ Configuração concluída.\nLogue na UI com usuário e senha 'admin'."
