3f1b92af9710b07a671a8d7e87bf9a423d57687a
Airton Carneiro <airtoncarneiro@gmail.com>
2025-04-14 21:25:24 -0300

    Update B3 variable names and add branching logic in ETL DAG

--- airflow/init-airflow.sh
+++ airflow/init-airflow.sh
@@ -5,12 +5,12 @@ set -e
 
 echo "⚙️ Configurando variáveis do Airflow..." 
 
-# Cria a variável B3_DOWNLOAD_ALL se não existir
-if ! airflow variables get B3_DOWNLOAD_ALL >/dev/null 2>&1; then
-    airflow variables set B3_DOWNLOAD_ALL "true" || { echo "Erro ao criar variável B3_DOWNLOAD_ALL"; exit 1; }
-    echo "✅ Variável B3_DOWNLOAD_ALL criada com valor 'true'!"
+# Cria a variável B3_DOWNLOAD_ALL_SERIES se não existir
+if ! airflow variables get B3_DOWNLOAD_ALL_SERIES >/dev/null 2>&1; then
+    airflow variables set B3_DOWNLOAD_ALL_ "true" || { echo "Erro ao criar variável B3_DOWNLOAD_ALL_"; exit 1; }
+    echo "✅ Variável B3_DOWNLOAD_ALL_ criada com valor 'true'!"
 else
-    echo "👤 Variável B3_DOWNLOAD_ALL já existe!"
+    echo "👤 Variável B3_DOWNLOAD_ALL_ já existe!"
 fi
 
 echo "🔐 Airflow configurado com sucesso!"
