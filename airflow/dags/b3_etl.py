0ddefcb44a78ecc3d3cbe1a6de808e15fcf4109c
Airton Carneiro <airtoncarneiro@gmail.com>
2025-04-18 09:57:34 -0300

    Remove código comentado e melhora a lógica de extração e download no DAG B3 ETL

--- airflow/dags/b3_etl.py
+++ airflow/dags/b3_etl.py
@@ -23,7 +23,6 @@ def download_zip_file(file_to_download: str) -> str:
     from src.services.file_handler import FileHandler
     logger = logging.getLogger("airflow.task")
     save_dir = os.path.join(os.path.dirname(__file__), "downloads")
-    # os.makedirs(save_dir, exist_ok=True)
     downloader = Downloader(logger=logger)
     downloaded_file = downloader._download_zip_file(file_to_download)
     saved_path = FileHandler._save_file_to_disk(downloaded_file, save_dir, logger=logger)
@@ -44,7 +43,6 @@ def extract_zip_files(zip_paths: List[str]) -> None:
 
 @task
 def serie_diaria(**context) -> List[str]:
-    # return ['COTAHIST_D01042025.ZIP']
     execution_date = context["execution_date"]
     dia_anterior = execution_date - timedelta(days=1)
     nome_arquivo = dia_anterior.strftime("COTAHIST_D%d%m%Y.ZIP")
@@ -52,21 +50,15 @@ def serie_diaria(**context) -> List[str]:
 
 @task
 def series_anuais(**context) -> List[str]:
-    # from airflow.models import Variable
-    # from datetime import datetime
-
     execution_date = context["execution_date"]
     ano_execucao = execution_date.year
 
     try:
-        # Tenta buscar a variável (sem valor padrão)
         ano_inicial_str = Variable.get("B3_CONFIG_DOWNLOAD_SERIE_ANUAL_DESDE_DE")
         ano_inicial = int(ano_inicial_str)
     except KeyError:
-        # Se não existir, usa o ano da execução
         ano_inicial = ano_execucao
 
-    # Gera os nomes dos arquivos entre ano_inicial e ano_execucao
     arquivos = [f"COTAHIST_A{ano}.ZIP" for ano in range(ano_inicial, ano_execucao + 1)]
     return arquivos
 
@@ -91,53 +83,38 @@ def series_anuais(**context) -> List[str]:
 def b3_etl():
     @task.branch(task_id="tipo_serie")
     def tipo_serie_choice():
-        # from airflow.models import Variable
-        B3_CONFIG_DOWNLOAD_SERIE = Variable.get("B3_CONFIG_DOWNLOAD_SERIE", default_var="series_anuais").lower()
-        # if B3_CONFIG_DOWNLOAD_SERIE == "serie_diaria":
-        #     return "grupo_diario.download_diario"
-        # else:
-        #     return "grupo_anual.download_anual"        
+        B3_CONFIG_DOWNLOAD_SERIE = Variable.get("B3_CONFIG_DOWNLOAD_SERIE", default_var="series_anuais").lower()     
         return "grupo_serie_diaria.download_diario" if B3_CONFIG_DOWNLOAD_SERIE == "serie_diaria" else "grupo_series_anuais.download_anual"
 
-    # Nós de controle
+    # Tasks de controle
     inicio = EmptyOperator(task_id="inicio")
     fim = EmptyOperator(task_id="fim")
-
     tipo_serie = tipo_serie_choice()
 
     # Grupo para série diária
     with TaskGroup(group_id="grupo_serie_diaria") as grupo_diario:
         arquivos_diarios = serie_diaria.override(task_id="download_diario")()
-        # with TaskGroup(group_id="download_serie_diaria") as downloads_diarios:
-        #     downloads_dia = download_zip_file.expand(file_to_download=arquivos_diarios)
-        # extrair_dia = extract_zip_files(downloads_dia)
-        # arquivos_diarios >> downloads_diarios >> extrair_dia
-
         downloads_dia = download_zip_file.expand(file_to_download=arquivos_diarios)
-        extrair_dia = extract_zip_files(downloads_dia)
-        arquivos_diarios >> downloads_dia >> extrair_dia
+        extrair_dia = extract_zip_files.expand(zip_paths=downloads_dia)
+        arquivos_diarios >> downloads_dia >> extrair_dia # type: ignore
 
     # Grupo para séries anuais
     with TaskGroup(group_id="grupo_series_anuais") as grupo_anual:
         arquivos_anuais = series_anuais.override(task_id="download_anual")()
-        # with TaskGroup(group_id="download_series_anuais") as downloads_anuais:
         downloads_ano = download_zip_file.expand(file_to_download=arquivos_anuais)
-        extrair_ano = extract_zip_files(downloads_ano)
-        arquivos_anuais >> downloads_ano >> extrair_ano
+        extrair_ano = extract_zip_files.expand(zip_paths=downloads_ano)
+        arquivos_anuais >> downloads_ano >> extrair_ano # type: ignore
 
-    # Conexões
-    # inicio >> tipo_serie
-    # tipo_serie >> grupo_diario >> fim
-    # tipo_serie >> grupo_anual >> fim
-
+    # Operador necessario pois quando o Airflow ignora um branch, ele não executa o fim
+    # Com este merge informando o trigger_rule, o Airflow executa o fim
     merge = EmptyOperator(
-    task_id="merge_paths",
-    trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS,
-)
-    inicio >> tipo_serie
-    tipo_serie >> grupo_diario >> merge
-    tipo_serie >> grupo_anual >> merge
-    merge >> fim
+        task_id="merge_paths",
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS,
+    )
+    inicio >> tipo_serie # type: ignore
+    tipo_serie >> grupo_diario >> merge # type: ignore
+    tipo_serie >> grupo_anual >> merge # type: ignore
+    merge >> fim # type: ignore
 
 
 dag = b3_etl()
